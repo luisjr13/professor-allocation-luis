4b3f3aaa4e05791fd451133d73aed42570f068ed
SALXJ <SALXJ@SALXJ-LCB7V0N3.OXBOW.COM>
2023-03-22 20:14:21 -0300

    Update22032023_3

--- src/test/java/professorallocationLuis/ProfessorRepositoryTest.java
+++ src/test/java/professorallocationLuis/ProfessorRepositoryTest.java
@@ -1,14 +1,15 @@
 package professorallocationLuis;
 
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.TestPropertySource;
 
 @DataJpaTest
-@AutoConfigureTestDatabase
-@Rollback
-@TestPropertySource
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+@TestPropertySource(locations = "classpath:application.properties")
 
 public class ProfessorRepositoryTest {
 
