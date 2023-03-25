bfb744a0dfc44ef45612e882d8fca9215dbc1566
SALXJ <SALXJ@SALXJ-LCB7V0N3.OXBOW.COM>
2023-03-25 14:15:58 -0300

    Aula25032023_4

--- src/test/java/Service/AllocationServiceTest.java
+++ src/test/java/Service/AllocationServiceTest.java
@@ -1,5 +1,13 @@
 package Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application.properties")
 public class AllocationServiceTest {
 
+    @Autowired
+    AllocationService allocationService;
 }
