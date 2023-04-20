6994f36fd3428d8909187b117d10486a9cf9c5f7
SALXJ <SALXJ@SALXJ-LCB7V0N3.OXBOW.COM>
2023-04-20 14:39:34 -0300

    Update_20042023

--- src/main/java/professorallocationLuis/ProfessorAllocationLuisApplication.java
+++ src/main/java/professorallocationLuis/ProfessorAllocationLuisApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProfessorAllocationLuisApplication {
 
+	//Teste
 	public static void main(String[] args) {
 		SpringApplication.run(ProfessorAllocationLuisApplication.class, args);
 	}
