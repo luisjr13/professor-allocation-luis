603017108686cac042becc0f4661f022740a22c2
SALXJ <SALXJ@SALXJ-LCB7V0N3.OXBOW.COM>
2023-04-20 19:31:38 -0300

    Update_20042023

--- src/main/java/professorallocationLuis/Config/CorsConfig.java
+++ src/main/java/professorallocationLuis/Config/CorsConfig.java
@@ -1,4 +1,4 @@
-package professorallocationLuis;
+package professorallocationLuis.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/professorallocationLuis/Config/SwaggerConfig.java
+++ src/main/java/professorallocationLuis/Config/SwaggerConfig.java
@@ -1,8 +1,9 @@
-package professorallocationLuis;
+package professorallocationLuis.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import professorallocationLuis.ProfessorAllocationLuisApplication;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
